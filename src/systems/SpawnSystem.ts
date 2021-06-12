0480976d1f874a256a302ca049afcacd1b2ab8f6
cquir <christian.quirouette@ryerson.ca>
2021-06-12 16:08:23 -0400

    bla

--- src/systems/SpawnSystem.ts
+++ src/systems/SpawnSystem.ts
@@ -1,44 +1,27 @@
 import bl from "blengine";
 import * as THREE from "three";
 import ecs, {Signature, System} from "blecs";
-
-// TODO:
-// Loop through a scripted list of 
-// shapes to send to the player for 
-// a given thing to build 
-// ex
-//
-// let shapes = [
-//     {
-//         type: "box",
-//         size: new THREE.Vector3(...),
-//         spawnPoint: new THREE.Vector3(...)
-//     }
-// ]
+import level1 from "./level1"
 
 // speed component might be nice
 const spawn = ({dt, entities} : any) => {
-    
     // 2% spawn rate per frame
-    if (Math.random() > 0.99) {
+    if (Math.random() > 0.99 && level1.shapes.length > 0) {
         let e = ecs.CreateEntity();
+        let spawn = level1.shapes.pop()
+        console.log(spawn)
         let geometry : any;
-        if (Math.random() < 0.5){  
-            geometry = new THREE.SphereGeometry(Math.random() + 0.5);
+        if (spawn?.type === "sphere"){  
+            geometry = new THREE.SphereGeometry(spawn?.radius);
         } else {
-            geometry = new THREE.BoxGeometry();
+            geometry = new THREE.BoxGeometry(spawn?.width,spawn?.height);
         }
-
         const material = new THREE.MeshPhongMaterial( { color: Math.random() * 0xffffff } );
         const shape = new THREE.Mesh( geometry, material );
-
         shape.position.set( (Math.random() -0.5) * 20, 0.5 +  Math.random(), Math.random() * 10 - 80);
-
         bl.scene.add(shape);
-
         ecs.AddComponent(e, "shape", shape);
     }
-
 }
 
 const SpawnSystem = new System(spawn);
