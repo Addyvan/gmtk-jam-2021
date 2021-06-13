b244ed46406e85a6dfb3b417d83bf2c00b680bfb
cquir <christian.quirouette@ryerson.ca>
2021-06-13 11:26:29 -0400

    broken scoring

--- src/tasks/scoring.ts
+++ src/tasks/scoring.ts
@@ -1,7 +1,48 @@
 import * as THREE from "three"
 import { Matrix3 } from "three";
 import player from "../player";
-import {groupToGrid} from "../utils"
+
+function translate(pos:THREE.Vector3,nt:number){
+    let translation = new THREE.Vector3(1,0,0)
+    if (nt==1){
+        translation.x = 1
+    }
+    if (nt==2){
+        translation.x = 2
+    }
+    if (nt==3){
+        translation.x += -1
+    }
+    if (nt==4){
+        translation.x += -2
+    }
+    if (nt==5){
+        translation.y += 1
+    }
+    if (nt==6){
+        translation.y += 2
+    }
+    if (nt==7){
+        translation.y += -1
+    }
+    if (nt==8){
+        translation.y += -2
+    }
+    if (nt==9){
+        translation.z += 1
+    }
+    if (nt==10){
+        translation.z += 2
+    }
+    if (nt==11){
+        translation.z += -1
+    }
+    if (nt==12){
+        translation.z += -2
+    }
+    pos.add(translation)
+    return pos
+}
 
 function rotate(pos:THREE.Vector3,nrot:number){
     let R = new Matrix3()
@@ -40,33 +81,34 @@ function rotate(pos:THREE.Vector3,nrot:number){
     return pos;
 }
 
-function scoring(voxels: Array<THREE.Vector3>) : number {
+function scoring(voxels: Array<THREE.Vector3>) : any {
     let fscore: number = 0;
-    for (let nrot=0;nrot<10;nrot++){
-        let score: number = 0;
-        for (let i=0; i< voxels.length; i++){
-            let voxel = new THREE.Vector3(voxels[i].x,voxels[i].y,voxels[i].z);
-            for (let j=0; j<player.children.length; j++){
-                let shape: any = player.children[j]
-                const box = new THREE.Box3();
-                let p1 = shape.position.clone();
-                p1 = rotate(p1,nrot);
-                let epsilon = 0.1;
-                let x = (p1.x <= voxel.x+epsilon) && (p1.x >= voxel.x-epsilon);
-                let y = (p1.y <= voxel.y+epsilon) && (p1.y >= voxel.y-epsilon);
-                let z = (p1.z <= voxel.z+epsilon) && (p1.z >= voxel.z-epsilon);
-                if (x && y && z){
-                    score += 1
-                }
-                else{
-                    score -= 1
+    for (let nt=0;nt<13;nt++){
+        for (let nrot=0;nrot<10;nrot++){
+            let score: number = 0;
+            for (let i=0; i< voxels.length; i++){
+                let voxel = new THREE.Vector3(voxels[i].x,voxels[i].y,voxels[i].z);
+                for (let j=0; j<player.children.length; j++){
+                    let shape: any = player.children[j]
+                    const box = new THREE.Box3();
+                    let p1 = shape.position.clone();
+                    console.log('p1 before translation',p1)
+                    p1 = translate(p1,nt)
+                    console.log('p1 after translation',p1)
+                    p1 = rotate(p1,nrot);
+                    let epsilon = 0.1;
+                    let x = (p1.x <= voxel.x+epsilon) && (p1.x >= voxel.x-epsilon);
+                    let y = (p1.y <= voxel.y+epsilon) && (p1.y >= voxel.y-epsilon);
+                    let z = (p1.z <= voxel.z+epsilon) && (p1.z >= voxel.z-epsilon);
+                    score += Number(x && y && z);
                 }
             }
-        }
-        if (score > fscore){
-            fscore = score;
+            if (score > fscore){
+                fscore = score;
+            }
         }
     }
+    console.log(fscore,player.children.length,voxels.length)
     fscore /= voxels.length;
     fscore /= 2;
     return fscore;
