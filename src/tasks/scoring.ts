295de6453cf8b5e7b304ceaaca3c2142fdca04b8
cquir <christian.quirouette@ryerson.ca>
2021-06-13 09:10:45 -0400

    fixed scoring

--- src/tasks/scoring.ts
+++ src/tasks/scoring.ts
@@ -1,32 +1,43 @@
 import * as THREE from "three"
 import { Matrix3 } from "three";
 import player from "../player";
+import {groupToGrid} from "../utils"
 
-function rotate(pos:THREE.Vector3,theta:number){
+function rotate(pos:THREE.Vector3,nrot:number){
     let R = new Matrix3()
-    theta *= 180/Math.PI;
-    R.set(Math.cos(theta),0,Math.sin(theta),0,1,0,-Math.sin(theta),0,Math.cos(theta));
+    R.set(1,0,0,0,1,0,0,0,1);
+    if (nrot==1){
+        R.set(0,0,1,0,1,0,-1,0,0);
+    }
+    if (nrot==2){
+        R.set(-1,0,0,0,1,0,0,0,-1);
+    }
+    if (nrot==3){
+        R.set(0,0,-1,0,1,0,1,0,0);
+    }
     pos.applyMatrix3(R);
     return pos;
 }
 
 function scoring(voxels: Array<THREE.Vector3>) : number {
     let fscore: number = 0;
-    for (let nrot=0;nrot<36;nrot++){
+    for (let nrot=0;nrot<4;nrot++){
         let score: number = 0;
         for (let i=0; i< voxels.length; i++){
             let voxel = new THREE.Vector3(voxels[i].x,voxels[i].y,voxels[i].z);
+            console.log('voxel',voxel);
             for (let j=0; j<player.children.length; j++){
                 let shape: any = player.children[j]
                 const box = new THREE.Box3();
                 let p1 = shape.position.clone();
-                let p2 = rotate(p1,10);
-                shape.position.x = p2.x;
-                shape.position.y = p2.y;
-                shape.position.z = p2.z;
-                shape.geometry.computeBoundingBox();
-                box.copy(shape.geometry.boundingBox).applyMatrix4(shape.matrixWorld);
-                score += Number(box.containsPoint(voxel));
+                p1 = rotate(p1,nrot);
+                console.log('p1',p1);
+                let epsilon = 0.1;
+                let x = (p1.x <= voxel.x+epsilon) && (p1.x >= voxel.x-epsilon);
+                let y = (p1.y <= voxel.y+epsilon) && (p1.y >= voxel.y-epsilon);
+                let z = (p1.z <= voxel.z+epsilon) && (p1.z >= voxel.z-epsilon);
+                console.log(x && y && z)
+                score += Number(x && y && z);
             }
         }
         if (score > fscore){
